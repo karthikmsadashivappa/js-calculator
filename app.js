609616ee0c20812318da2b521a9af5f8cde9fd95
Karthik M S <ms18karthik@gmail.com>
2019-10-09 06:35:28 +0530

    adding math operations

--- app.js
+++ app.js
@@ -12,14 +12,58 @@ function buttonClick(value) {
     // is a number;
     handleNumber(value);
   }
+  screen.innerText = buffer;
 }
 
-function handleSymbol() {}
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
+  if (buffer === "0") {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
 function handleNumber(numberString) {
   if (buffer === "0") {
     buffer = numberString;
   } else {
-    buffer += buffer + numberString;
+    buffer += numberString;
   }
 }
 
@@ -27,7 +71,6 @@ function init() {
   document
     .querySelector(".calc-buttons")
     .addEventListener("click", function(event) {
-      console.log(event);
       buttonClick(event.target.innerText);
     });
 }
